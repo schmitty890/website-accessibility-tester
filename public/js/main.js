59050da18459608102d3faf936f50606bbcd898a
Jason Schmitt <jasonschmitt@Jasons-MBP-2.lan>
2024-06-05 21:25:50 -0400

    passing front end url string to backend

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
-console.log('works');
+// Fetch accessibility issues
+const testAccessibility = async (e) => {
+  e.preventDefault();
+  const url = document.querySelector('#url').value;
+  console.log(url);
+};
+// Add issues to the DOM
+
+// Set loading state
+
+// Excape HTML
+
+document.querySelector('#form').addEventListener('submit', testAccessibility);
